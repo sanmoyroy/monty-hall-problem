7783c7483254730500098c755b6e08268bdfd72c
Sanmoy Roy <sanmoyroy@gmail.com>
2022-12-08 17:12:24 +0100

    App frontend - restructure and add tests

--- app-frontend/src/components/SimulationForm/SimulationForm.test.js
+++ app-frontend/src/components/SimulationForm/SimulationForm.test.js
@@ -2,5 +2,9 @@ import { render, screen } from '@testing-library/react';
 import SimulationForm from './SimulationForm';
 
 test('renders number of simulations input field', () => {
-  render(<SimulationForm />);
+  const data = {'results': [], totalWins: 0};
+  const setData = () => {};
+  render(<SimulationForm setData={setData}/>);
+  const numberOfSimulationTextBox = screen.getAllByPlaceholderText('Number of simulations');
+  expect(numberOfSimulationTextBox).toBeTruthy();
 });
